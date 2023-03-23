2969dd82affd2bc75909bc21e882717c5d5eaad0
gen-shun <108863741+gen-shun@users.noreply.github.com>
2023-03-23 22:49:10 +0900

    Update HelloController.java
    string update

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu! Tanzu! Tanzu!";
+		return "Greetings from Spring Boot + Tanzu! Tanzu! Tanzu! Tanzu! Tanzu!";
 	}
 
 }
